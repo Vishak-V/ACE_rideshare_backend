8108e63e288b0e7fad647385725e1f7a2ff98ba8
vishakvik <vishak20903@gmail.com>
2024-08-13 14:34:57 -0500

    added middleware to allow requests from any domain

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from .database import engine
 from passlib.context import CryptContext
 from .routers import user,trip,auth,join
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
@@ -11,6 +12,16 @@ pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(trip.router)
